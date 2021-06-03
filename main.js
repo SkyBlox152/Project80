8317bd6b59b10caeedfbd0ec5241088632771508
SkyBlox152 <74969893+SkyBlox152@users.noreply.github.com>
2021-06-03 16:09:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ inputs.push(document.getElementById("ip"+i).value);
 document.getElementById("finalpara").innerHTML = inputs.join(".")
 }
 }
+
+function getParagraph2(){
+for(var i=1; 1<=6; i++){
+inputs.push(document.getElementById("id"+i).value);
+document.getElementById("finalpara2").innerHTML = inputs.join(".")
+}
+}
